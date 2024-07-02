e6c9283203147e98bf59fa4f4f3fa58848897182
Ali Osman Öztürk <developer@aliosmanozturk.me>
2024-07-03 01:11:34 +0300

    Add RegisterView.swift

---
+++ SwiftUI-Firebase/SwiftUI-Firebase/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  SwiftUI-Firebase
+//
+//  Created by Ali Osman Öztürk on 3.07.2024.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    RegisterView()
+}
