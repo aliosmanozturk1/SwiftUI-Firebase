e9ca38e1ee01ce3bb2c5253edd9a133e9ae2873e
Ali Osman Öztürk <developer@aliosmanozturk.me>
2024-07-03 00:51:52 +0300

    Create Xcode SwiftUI projects

---
+++ SwiftUI-Firebase/SwiftUI-Firebase/SwiftUI_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FirebaseApp.swift
+//  SwiftUI-Firebase
+//
+//  Created by Ali Osman Öztürk on 3.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
