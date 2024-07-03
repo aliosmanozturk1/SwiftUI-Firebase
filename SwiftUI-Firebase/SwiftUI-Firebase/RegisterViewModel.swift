9574f47bf5cc54bb0c94c72494b67ac005035080
Ali Osman Öztürk <developer@aliosmanozturk.me>
2024-07-03 09:41:20 +0300

    Update RegisterViewModel.swift

--- SwiftUI-Firebase/SwiftUI-Firebase/RegisterViewModel.swift
+++ SwiftUI-Firebase/SwiftUI-Firebase/RegisterViewModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class RegisterViewModel: ObservableObject {
+    @Published var email = ""
+    @Published var password = ""
+    @Published var errorMessage = ""
+    @Published var isRegistered = false
+    
+    func register() {
+        FirebaseManager.shared.auth.createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                self.errorMessage = error.localizedDescription
+            } else if let authResult = authResult {
+                self.isRegistered = true
+                
+            }
+            
+            
+            
+        }
+    }
+}
