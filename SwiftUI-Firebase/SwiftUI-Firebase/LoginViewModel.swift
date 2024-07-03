c8d5ffcd91bc3ce44ca071f964d0db7ddc43ef42
Ali Osman Öztürk <developer@aliosmanozturk.me>
2024-07-03 10:20:01 +0300

    Update LoginViewModel.swift

--- SwiftUI-Firebase/SwiftUI-Firebase/LoginViewModel.swift
+++ SwiftUI-Firebase/SwiftUI-Firebase/LoginViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class LoginViewModel: ObservableObject {
+    @Published var email = ""
+    @Published var password = ""
+    @Published var errorMessage = ""
+    @Published var isLoggedIn = false
+    
+    func login() {
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                self.errorMessage = error.localizedDescription
+            } else if let result = result {
+                self.isLoggedIn = true
+                let user = User(uid: result.user.uid, email: result.user.email)
+                
+                print("Successfully logged in as user: \(user)")
+            }
+        }
+    }
+}
