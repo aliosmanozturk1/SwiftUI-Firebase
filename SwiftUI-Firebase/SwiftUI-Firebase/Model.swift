6657940e5335c8f50ff2e1f0960333f885498bb9
Ali Osman Öztürk <developer@aliosmanozturk.me>
2024-07-03 09:45:01 +0300

    Add Model.Swift

---
+++ SwiftUI-Firebase/SwiftUI-Firebase/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  SwiftUI-Firebase
+//
+//  Created by Ali Osman Öztürk on 3.07.2024.
+//
+
+import Foundation
+
+struct User {
+    let uid: String
+    let email: String?
+}
